42d362976e277e1c3878e03d8684324fc3b9152b
Eugene Sukhomlyn <nemesises@live.com>
2022-01-28 12:53:59 +0200

    initial  commit with layout

---
+++ url_shortener/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, InputTextModule, AppRoutingModule, ButtonModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
