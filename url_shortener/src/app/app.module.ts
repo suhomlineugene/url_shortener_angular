19cb109aec239a8926102a5a192d711d829e393d
Eugene Sukhomlyn <nemesises@live.com>
2022-01-28 12:58:47 +0200

    table for shortened links

--- url_shortener/src/app/app.module.ts
+++ url_shortener/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, InputTextModule, AppRoutingModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    AppRoutingModule,
+    ButtonModule,
+    TableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
